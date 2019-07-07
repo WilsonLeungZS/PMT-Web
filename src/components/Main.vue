7153462a8a49504b32989041449e58337b9d7d2c
Wilson Leungzs <wilson_leungzs@163.com>
2019-07-07 11:47:50 +0800

    First Commit

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
