bd3a445ee780c8503e4fb83b126d7115cda0a36c
Wilson Leungzs <wilson_leungzs@163.com>
2020-03-11 18:14:31 +0800

    Update

--- src/components/Task/TaskList.vue
+++ src/components/Task/TaskList.vue
@@ -109,7 +109,7 @@
               <el-table-column prop="task_assignee" label="Assignee" align="center" width="180px" v-if="!taskListRule.showColForLv1" key="17"></el-table-column>
               <el-table-column prop="task_issue_date" label="Issue Date" align="center" width="180px" v-if="!taskListRule.showColForLv1" key="18"></el-table-column>
               <el-table-column prop="task_target_complete" label="Target Completion Date" align="center" width="190px" v-if="!taskListRule.showColForLv1" key="19"></el-table-column>
-              <el-table-column fixed="right" label="Plan Mode" align="center" width="95px" v-if="taskListRule.showColForLv1" >
+              <el-table-column fixed="right" label="Plan" align="center" min-width="60px" v-if="taskListRule.showColForLv1" >
                 <template slot-scope="scope">
                   <el-button @click="startPlanTask(scope.row)" :style="{'border': 'none', 'color': 'white'}" type="warning" size="small" icon="el-icon-s-flag"></el-button>
                 </template>
