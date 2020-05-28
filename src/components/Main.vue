a8c57c2fe9a1c3c0281c005434e048a43b17720d
WilsonLeungZS <50231918+WilsonLeungZS@users.noreply.github.com>
2020-05-28 14:08:33 +0800

    Revert "update My View.vue and PrjManagement.vue-userList"

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,7 +11,6 @@
                   <el-dropdown-item icon="el-icon-date" command="timesheet" class="main-menu-dropdown-text">Timesheet</el-dropdown-item>
                   <el-dropdown-item icon="el-icon-tickets" command="task" class="main-menu-dropdown-text">Task</el-dropdown-item>
                   <el-dropdown-item icon="el-icon-edit-outline" command="management" class="main-menu-dropdown-text">Management</el-dropdown-item>
-                  <el-dropdown-item icon="el-icon-edit-outline" command="myView" class="main-menu-dropdown-text">MyView</el-dropdown-item>
                   <el-dropdown-item icon="el-icon-paperclip" command="others" class="main-menu-dropdown-text" v-if="false">Others</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
@@ -163,9 +162,6 @@ export default {
       }
       else if (command === 'management') {
         this.$router.push({path: '/Management'})
-      }
-      else if (command === 'myView') {
-        this.$router.push({path: '/MyView/AssignToMe'})
       }
       else if (command === 'others') {
         this.$router.push({path: '/Others'})
