06ec8fa148f69663f9965e60802ad2f40e4ee73b
celineismyname <45379439+celineismyname@users.noreply.github.com>
2020-06-17 09:36:48 +0800

    fix regular task bug and build up"My Information" form

--- src/components/Task/TaskList.vue
+++ src/components/Task/TaskList.vue
@@ -2398,7 +2398,8 @@ export default {
             // })
           } else {
             console.log('Not Pool Task')
-            this.$data.lv3TaskItemRule.showRespLeader = tthis.$data.lv3TaskItemRule.disableAssignee = falserue
+            this.$data.lv3TaskItemRule.showRespLeader = true
+            this.$data.lv3TaskItemRule.disableAssignee = false
             this.$data.lv3TaskItemRule.showSubTaskEst = true
             this.$data.lv3TaskItemRule.showTypeTag = true
             this.$data.lv3TaskItemRule.showDeliverableTag = true
